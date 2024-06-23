2c1cc2b3ed949417a94376361045d5b5be896120
Anu Bharti <anubharti031@gmail.com>
2024-06-23 08:48:46 +0530

    python program

---
+++ Turtle.py
@@ -0,0 +1,10 @@
+# Print Turtles in Python :
+
+import turtle
+turtle.bgcolor("black")
+squary=turtle.Turtle()
+squary.speed(20)
+squary.pencolor("red")
+for i in range(2000):
+    squary.backward(i)
+    squary.left(91)
